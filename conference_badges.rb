affa55ddefc82c2186ac36fb66b5e28785c934cd
Fillan Lee <fillanlee@aol.com>
2019-02-08 05:28:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,44 @@
 # Write your code here.
+def badge_maker(name)
+  greet = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+   index = 0
+  new_array = []
+ array_less_one = array_of_names.count - 1
+ 
+  while index <= array_less_one
+  new_array << badge_maker(array_of_names[index])
+  index += 1
+end
+ new_array
+end
+
+def assign_rooms(array_of_names)
+  room_num = 1
+  list_assign_rooms = []
+  
+  while room_num <= array_of_names.length
+  list_items = "Hello, #{array_of_names[room_num-1]}! You'll be assigned to room #{room_num}!"
+  list_assign_rooms << list_items 
+  room_num += 1
+  end
+list_assign_rooms
+end
+
+def printer (attendees)
+  # index = 0
+  # while index <= 1
+  #   putts = attendees[1]
+  #     puts batch_badge_creator(putts)
+  #   index += 1
+  
+  # end
+  new_list = []
+  list = batch_badge_creator(attendees)
+  new_list << list.flatten
+  
+  puts new_list[1]
+
+end
