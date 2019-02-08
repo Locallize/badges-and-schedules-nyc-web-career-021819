064e87118ae82a8564685ce7303b02b3267773f0
Fillan Lee <fillanlee@aol.com>
2019-02-08 06:17:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -32,7 +32,7 @@ def printer (attendees)
   counter = 0 
   while counter < attendees.length
   name = attendees
-  puts assign_rooms(name)
+ 
   puts badge_maker(attendees[counter])
   
   counter += 1
